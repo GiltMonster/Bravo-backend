d53d7f0eea95ec1b2b41126632133fc7895a38e3
GiltMonster <lucassantoslukas2@gmail.com>
2024-11-24 14:36:52 -0300

    Feat: status do pedido criado para incrementar o detalhamento no resource do pedido item.

---
+++ app/Models/PedidoStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PedidoStatus extends Model
+{
+    use HasFactory;
+
+    protected $table = 'PEDIDO_STATUS';
+    protected $primaryKey = 'STATUS_ID';
+    public $timestamps = false;
+
+}
